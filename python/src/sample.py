b7beb5ac8a39a8b9fb871aab513e406257ab3bdb
Takahiro Konno <t.konno@shishimaro.co.jp>
2021-06-17 06:07:44 +0900

    first commit

---
+++ python/src/sample.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x = np.range(0, 100, 0.5)
+
+hz = 5.
+y = np.sin(2.0 * np.pi * (x * hz) / 100)
+
+# グラフを描画
+plt.plot(x, y)
+plt.savefig('test.png')
